3f2bb515b2aca18b1c517eedd3c956060dd86c0b
zkan <zkan.cs@gmail.com>
2024-10-27 10:04:47 +0700

    Install comment and indent blankline + set more keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,11 @@ local opts = { noremap = true, silent = true }
 -- Directory navigation
 keymap.set("n", "<C-n>", ":Neotree toggle left<CR>", opts)
 keymap.set("n", "<leader>n", ":Neotree toggle left<CR>", opts)
+
+-- Indenting
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
+
+-- Comments (_ represents /)
+vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
+vim.api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
