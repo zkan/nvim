0e6edfe18116c7726d67fbb4bf3a9704370cce81
zkan <zkan.cs@gmail.com>
2024-11-02 11:23:49 +0700

    Map keys for Rails

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,3 +13,21 @@ keymap.set("v", ">", ">gv")
 -- Comments (_ represents /)
 vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
 vim.api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
+
+-- Rails
+local run_test = function()
+  local current_file = vim.fn.expand('%:p')
+  local test_string = "silent !zellij run -d Down -n 'testing' -- bin/rails test " .. current_file
+  vim.api.nvim_command(test_string)
+end
+
+local run_all_tests = function()
+  local test_string = "silent !zellij run -d Down -n 'testing' -- bin/rails test"
+  vim.api.nvim_command(test_string)
+end
+
+vim.api.nvim_create_user_command('RunTest', run_test, {})
+vim.api.nvim_create_user_command('RunAllTests', run_all_tests, {})
+
+vim.keymap.set("n", "<leader>t", ":RunTest<CR>", { desc = "run current file test" })
+vim.keymap.set("n", "<leader>a", ":RunAllTests<CR>", { desc = "run all tests" })
