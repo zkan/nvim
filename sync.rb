9bf6d7dfd4ab75bca5787df96120e9c71ac99583
zkan <zkan.cs@gmail.com>
2024-11-02 15:49:35 +0700

    Fix copying folder cmd

--- sync.rb
+++ sync.rb
@@ -1,3 +1,4 @@
+system("mkdir -p ~/.config/nvim/lua/{config,plugins}")
 system("cp init.lua ~/.config/nvim/init.lua")
 system("cp lazy-lock.json ~/.config/nvim/lazy-lock.json")
-system("cp -R lua ~/.config/nvim/lua")
+system("cp -R lua ~/.config/nvim/")
